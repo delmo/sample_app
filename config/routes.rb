7aaff2586d5671efead7defb92d4a3dbee2052c3
Ryan Delmo <rtgd20@sussex.ac.uk>
2013-11-13 23:40:02 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  # get "users/new", not needed this as "resources :users" generates all RESTful
+  resources :users
   root 'static_pages#home'
   match '/signup', to: 'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
